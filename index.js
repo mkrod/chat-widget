c4c190262e6743b607b07de632e0d3de27f3081a
MkRoD <mkrodsullivan@gmail.com>
2025-06-08 22:07:50 +0100

    Commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const protocol = "https";
 const server_uri = "192.168.43.103:3000";
 const cssPath = "https://helpful-madeleine-61e48d.netlify.app/index.css";    // path to the css file
 const NotificationUrl = "./assets/notif-audio.mp3";
-const ServerfileDir = "file:///C:/Users/MkRoD/Desktop/Alquify-server/temp"; //replace with url of dir where the file are stored
+const ServerfileDir = `${protocol}://${server_uri}/chat_assets`; //replace with url of dir where the file are stored
 const userDPurl = "https://cdn.vectorstock.com/i/750p/31/67/robot-icon-bot-sign-design-chatbot-symbol-vector-27973167.avif";
 /*
 const wScript = document.createElement("script");
